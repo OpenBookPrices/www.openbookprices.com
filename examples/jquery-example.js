74df284c720c20d8d37d340a6e298d8aceb16d69
Edmund von der Burg <evdb@ecclestoad.co.uk>
2014-03-09 12:35:40 -0300

    Add lots of comments

--- examples/jquery-example.js
+++ examples/jquery-example.js
@@ -1,3 +1,13 @@
+/*
+
+  TODO
+
+  * add url to offer block
+  * ? add signature to offer entry?
+  * ? add condition to offer entry?
+
+*/
+
 
 $(function () {
 
@@ -11,22 +21,34 @@ $(function () {
   var pendingList = $('#pending');
   var resultsList = $('#results');
 
+  /*
+    Callback to handle the initial request for all prices. The response
+    includes an array of results which will be identical to those returned by
+    the vendor specific price queries. This means that mostly we just need to
+    pass those results to the same code that would handle the vendor specific
+    response.
+  */
   var handlePricesResponse = function (response) {
-    var request = response.request;
-    var results = response.results;
 
-    // FIXME - show currency and country details
-    console.log(request);
+    // Remove all the contents of the two result lists
+    pendingList.find('li').remove();
+    resultsList.find('li').remove();
 
-    for (var i = 0; i < results.length; i++) {
-      handleVendorResponse(results[i]);
-    }
+    // Go through all the results and pass them to the handleVendorResponse
+    // callback as they are the same as you'd get from the vendor specific
+    // price request.
+    _.each(response.results, handleVendorResponse);
+
+    // FIXME - show country and currency details
 
   };
 
   var displayOffer = function (offerRow, offer) {
+
     // Insert the rows in order with the cheapest first. Find the first
-    // row that is more expensive than this offer.
+    // row that is more expensive than this offer. If found insert before it,
+    // otherwise at end of list.
+
     var more_expensive_row = resultsList.find('li').filter(function (index) {
       var row = $(this);
       var total = parseFloat( row.data().totalPrice );
@@ -63,10 +85,16 @@ $(function () {
       displayOffer(offerRow, offer);
     });
 
-    // if required start another fetch
+    // Whilst fetching data we'll show a pending indicator per vendor. Work out
+    // what ID this should have.
     var pendingID = '#pending-' + response.vendor.code;
+
+    // If another fetch is needed (eg data is old, or not available yet) retryDelay will be true.
     if (response.meta.retryDelay) {
 
+      // Another fetch is required for better data.
+
+      // If not already being shown display a pending fetch indicator.
       if (!$(pendingID).size()) {
         var pendingRow = $(
           _.template(
@@ -77,17 +105,19 @@ $(function () {
         pendingRow.appendTo(pendingList);
       }
 
+      // Use setTimeout to delay the next fetch as required.
       window.setTimeout(
         function () {
           $.get(
-            response.request.url,
+            response.request.url, // url to query was provided by API
             handleVendorResponse
           );
-
         },
-        response.meta.retryDelay*1000
+        response.meta.retryDelay * 1000 // delay is provided by API
       );
     } else {
+      // Data is good, no subsequent fetch required. Remove any pending
+      // indicator.
       $(pendingID).remove();
     }
 
@@ -99,18 +129,20 @@ $(function () {
     // Prevent the form from submitting to the server.
     e.preventDefault();
 
-    pendingList.find('li').remove();
-    resultsList.find('li').remove();
-
     // Get the isbn number from the form field
     var isbn = $("#isbn-input").val();
 
-    // Send a request to the API
+    // Send a request to the API for prices from all vendors. Note that there
+    // is no country or currency specified - let the server determine that from
+    // the IP address and then redirect (which jQuery will follow for us).
+    // Likewise let the server tidy up the isbn number and 404 or redirect as
+    // required.
     $.get(
       api_base + "/v1/books/" + isbn + "/prices",
       handlePricesResponse
     );
 
+    // FIXME - show currency and country details
 
   });
 });
